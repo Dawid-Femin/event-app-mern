3a6e8dd623e57c3d21ab0bbfcb25e268faeed610
Dawid Femin <dawid.femin@gmail.com>
2022-12-04 00:40:58 +0100

    Created JSX structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,31 @@
+//Imported components
+import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
+import events from './images/events.png';
+import Form from './components/Form/Form';
+import Posts from './components/Posts/Posts';
+
+//Imported style
 import './App.css';
 
 function App() {
   return (
     <>
-    <h1>App running</h1>
+    <Container maxWidth='lg' />
+      <AppBar className='appBar' position='static' color='inherit'>
+        <Typography className='heading' variant='h2' align='center'>Events <img className='image' src={ events } alt="events" height="60" /> </Typography>
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </>
   );
 }
